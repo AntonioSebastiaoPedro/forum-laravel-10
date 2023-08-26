898e97c9dbf0d753a5ba82e50455fb9f005ca934
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2023-08-26 20:12:02 +0100

    Listando os suportes

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,20 @@
-<h1>Listagem dos Supports</h1>
+<h1>Listagem dos Suportes</h1>
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th>Acções</th>
+    </thead>
+    <tbody>
+    @foreach ($supports as $support)
+        <tr>
+            <td>{{ $support->subject }}</td>
+            <td>{{ $support->status }}</td>
+            <td>{{ $support->body }}</td>
+            <td>-</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\SupportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
